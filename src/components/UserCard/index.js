b254b43e46217af5d45e95447995a01d78387533
z-shaffer <zshaffer@protonmail.com>
2024-04-17 00:44:48 +0200

    Refactoring UserCards into its own component

---
+++ src/components/UserCard/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {Text, ImageBackground, View, StyleSheet} from 'react-native';
+
+const UserCard = () => {
+  return (
+    <View style={styles.card}>
+      <ImageBackground
+        source={{
+          uri: 'https://img.a.transfermarkt.technology/portrait/big/418560-1709108116.png',
+        }}
+        style={styles.image}>
+        <View style={styles.cardInner}>
+          <Text style={styles.name}>Erling Haaland</Text>
+          <Text style={styles.bio}>I score goal</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+  },
+  name: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  bio: {
+    fontSize: 18,
+    color: 'white',
+    lineHeight: 25,
+  },
+});
+
+export default UserCard;
