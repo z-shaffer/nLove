814358fcc0986914224c1e9c38c3b42ca31cd5ae
z-shaffer <zshaffer@protonmail.com>
2024-04-17 00:33:14 +0200

    User card complete

--- App.js
+++ App.js
@@ -1,22 +1,72 @@
 import React from 'react';
-import { Text, Image, View, StyleSheet } from 'react-native';
+import {Text, Image, ImageBackground, View, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <Image source={{
-        uri: 'https://img.a.transfermarkt.technology/portrait/big/418560-1709108116.png'
-      }} 
-      style={{
-        width: 250, height: 250
-      }} 
-      />
+    <View style={styles.pageContainer}>
+      <View style={styles.card}>
+        <ImageBackground
+          source={{
+            uri: 'https://img.a.transfermarkt.technology/portrait/big/418560-1709108116.png',
+          }}
+          style={styles.image}>
+          <View style={styles.cardInner}>
+            <Text style={styles.name}>Erling Haaland</Text>
+            <Text style={styles.bio}>I score goal</Text>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  
-})
+  pageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+    elevation: 11,
+  },
+  name: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  bio: {
+    fontSize: 18,
+    color: 'white',
+    lineHeight: 25,
+  },
+});
 
 export default App;
