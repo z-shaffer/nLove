9c54c37d8bf5f35bc0135934f40deaa313447d2b
z-shaffer <zshaffer@protonmail.com>
2024-04-18 14:09:54 +0200

    Swipe animation init

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
 import UserCard from './src/components/UserCard/';
 import users from './assets/data/users';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import 'react-native-gesture-handler';
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+} from 'react-native-gesture-handler';
 
 const App = () => {
+  const translateX = useSharedValue(0);
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+    ],
+  }));
+  const gestureHandler = useAnimatedGestureHandler({
+    oneStart: _ => {
+      console.warn('Touch started');
+    },
+    onActive: event => {
+      translateX.value = event.translationX;
+      console.warn('Touch x: ', event.translationX);
+    },
+    onEnd: () => {
+      console.warn('Touch ended');
+    },
+  });
+
   return (
-    <View style={styles.pageContainer}>
-      <UserCard user={users[0]} />
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.pageContainer}>
+        <PanGestureHandler onGestureEvent={gestureHandler}>
+          <Animated.View style={[styles.animatedCard, cardStyle]}>
+            <UserCard user={users[0]} />
+          </Animated.View>
+        </PanGestureHandler>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
@@ -16,6 +54,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  animatedCard: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
