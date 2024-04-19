fd697ef5787993e0f0417a5346f715442093f96c
z-shaffer <zshaffer@protonmail.com>
2024-04-19 22:27:21 +0200

    Card stack animation + documentation update

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
 import React, {useState} from 'react';
 import UserCard from './src/components/UserCard/';
 import users from './assets/data/users';
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  useWindowDimensions,
-  View,
-} from 'react-native';
+import {StyleSheet, useWindowDimensions, View} from 'react-native';
 import Animated, {
   interpolate,
   useAnimatedGestureHandler,
@@ -22,20 +16,43 @@ import {
 } from 'react-native-gesture-handler';
 
 const ROTATION = 60;
+const SWIPE_VELOCITY = 800;
 
 const App = () => {
+  // Index of users in the stack
   const [currentIndex, setCurrentIndex] = useState(0);
   const [nextIndex, setNextIndex] = useState(1);
+  // Reference to profile of users in the stack
   const currentProfile = users[currentIndex];
   const nextProfile = users[nextIndex];
+  // Calculate screen dimensions for animations
   const {width: screenWidth} = useWindowDimensions();
   const hiddenTranslateX = 2 * screenWidth;
+  // Current X position of top card on stack -- zero by default.
   const translateX = useSharedValue(0);
+  // Interpolation for scale of next card when swiping
+  const scale = useDerivedValue(() =>
+    interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.8, 1],
+    ),
+  );
+  // Interpolation for opacity of next card when swiping
+  const opacity = useDerivedValue(() =>
+    interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.5, 1],
+    ),
+  );
+  // Interpolation for rotation of current card when swiping
   const rotate = useDerivedValue(
     () =>
       interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
       'deg',
   );
+  // Animation style for current card
   const cardStyle = useAnimatedStyle(() => ({
     transform: [
       {
@@ -46,16 +63,30 @@ const App = () => {
       },
     ],
   }));
+  // Animation style for next card
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: scale.value,
+      },
+    ],
+    opacity: opacity.value,
+  }));
+  // Gesture handler for swiping on current card
   const gestureHandler = useAnimatedGestureHandler({
     onStart: (_, context) => {
+      // Store starting value as a reference point
       context.startX = translateX.value;
     },
     onActive: (event, context) => {
       translateX.value = context.startX + event.translationX;
       console.warn('Touch x: ', event.translationX);
     },
-    onEnd: () => {
-      //if (translateX.value <= -200 )
+    onEnd: event => {
+      // Absolute value required due to left swipes having a negative value
+      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
+        translateX.value = 0;
+      }
       console.warn('Touch ended');
     },
   });
@@ -64,7 +95,9 @@ const App = () => {
     <GestureHandlerRootView>
       <View style={styles.pageContainer}>
         <View style={styles.nextCardContainer}>
-          <UserCard user={nextProfile} />
+          <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+            <UserCard user={nextProfile} />
+          </Animated.View>
         </View>
         <PanGestureHandler onGestureEvent={gestureHandler}>
           <Animated.View style={[styles.animatedCard, cardStyle]}>
