ca8d4ae7b6205e602676708bb2e52734fa2386e3
z-shaffer <zshaffer@protonmail.com>
2024-04-22 15:17:20 +0200

    Logo init

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, SafeAreaView} from 'react-native';
 
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 import HomeScreen from './src/screens/HomeScreen';
+import MatchesScreen from './src/screens/MatchesScreen';
+import Logo from './src/components/Logo';
 
 const App = () => {
+  const color = '#fefefe';
   return (
-    <GestureHandlerRootView>
-      <View style={styles.pageContainer}>
-        <HomeScreen />
-      </View>
-    </GestureHandlerRootView>
+    <SafeAreaView style={{flex: 1}}>
+      <GestureHandlerRootView>
+        <View style={styles.pageContainer}>
+          <FontAwesome name="cloud" size={60} color={color} />
+          <MatchesScreen />
+        </View>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 };
 
@@ -20,6 +31,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+    backgroundColor: '#7e57c2',
+    paddingTop: 10,
   },
 });
 
