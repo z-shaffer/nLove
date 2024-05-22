1210c1f3c7a58b5cb92d12b9bbb6ecf27b6782db
z-shaffer <zshaffer@protonmail.com>
2024-05-22 17:19:34 +0200

    S3 support for images added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s34ff09c64": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,6 +1,114 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getMatch = /* GraphQL */ `
+  query GetMatch($id: ID!) {
+    getMatch(id: $id) {
+      id
+      User1ID
+      User2ID
+      isMatch
+      User1 {
+        id
+        name
+        images
+        bio
+        gender
+        lookingFor
+        sub
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      User2 {
+        id
+        name
+        images
+        bio
+        gender
+        lookingFor
+        sub
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        __typename
+      }
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+      matchUser1Id
+      matchUser2Id
+      __typename
+    }
+  }
+`;
+export const listMatches = /* GraphQL */ `
+  query ListMatches(
+    $filter: ModelMatchFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listMatches(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        User1ID
+        User2ID
+        isMatch
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        matchUser1Id
+        matchUser2Id
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
+export const syncMatches = /* GraphQL */ `
+  query SyncMatches(
+    $filter: ModelMatchFilterInput
+    $limit: Int
+    $nextToken: String
+    $lastSync: AWSTimestamp
+  ) {
+    syncMatches(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      lastSync: $lastSync
+    ) {
+      items {
+        id
+        User1ID
+        User2ID
+        isMatch
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+        matchUser1Id
+        matchUser2Id
+        __typename
+      }
+      nextToken
+      startedAt
+      __typename
+    }
+  }
+`;
 export const getUser = /* GraphQL */ `
   query GetUser($id: ID!) {
     getUser(id: $id) {
